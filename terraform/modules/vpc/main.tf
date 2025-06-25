a46c9f1425003ffe971f6dc130498ba8636f6f72
Robert Bulmer <r95@hotmail.co.uk>
2025-06-25 17:50:53 +0100

    no outbound internet traffic from sg

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "endpoint_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [aws_vpc.app_vpc.cidr_block]
   }
   tags = merge(var.tags, {
     Name = "MongoDB Atlas Security Group"
